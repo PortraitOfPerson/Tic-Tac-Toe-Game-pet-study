748207db4369d73a56418aee55519519f0ad1c10
PortraitOfPerson <portraitofperson@gmail.com>
2023-10-09 19:32:08 +0300

    Second and last commit

--- ttt-game.py
+++ ttt-game.py
@@ -1,31 +1,72 @@
-#The tic-tac-toe game by Nikita Kharchenko 
-#A pet-training-project (the code was not invented by me, the project was taken for training purposes)
-#No Pygame,draw and play in terminal
+#The tic-tac-toe game, a pet-training-project 
+#No pygame,draw and play in terminal
+#I guess its a worst way to write this game...
 
 
 field_size = 3
 field = [1,2,3,4,5,6,7,8,9]
 
 
-
+#draw a field by a hand(yea,just training press buttons)
 def draw_field():
-    print('_' * 4 * field_size)
     for i in range(field_size):
-        print(" " * 3 + "│")
-        print(field[i*field_size],'│')
+        print('_' * 4 * field_size)
+        print((" " * 3 + "│") *3)
+        print("",field[i*field_size], '│', field[1 + i*3], '│', field[2 + i*3],'│')
+        print(("_" * 3 + "│") *3)
         
         
         
-        
-def game_action():
+def game_action(index,char):
     #Doing action
-    pass
+    if index > 9 or index < 1 or field[index-1] in ('X','O'):
+        return False
+    
+    field[index-1] = char
+    return True
 
 def check_win():
-    pass
+    win = False
+    win_combination = (
+        (0,1,2),(3,4,5),(6,7,8),(0,4,8),(0,3,6),(1,4,7),(2,5,8),(2,4,6)
+    )
+    
+    for pos in win_combination:
+        if (field[pos[0]] == field[pos[1]] and field[pos[1]] == field[pos[2]]):
+            win = field[pos[0]]
+    return win
 
 def start_game():
+    current_player = 'X'
+    step = 1
     draw_field()
+    
+    
+    while (step<10) and (check_win() == False):
+        index = input('Player ' + current_player + ' turn!' + ' Enter field number (0 - quit)')
+        if (index == "0"):
+            break
+        
+        
+        if (game_action(int(index),current_player)):
+            print('Good turn')
+            
+            if (current_player == 'X'):
+                current_player = 'O'
+            else:
+                current_player = 'X'
+            
+            draw_field()
+            step += 1
+        else:
+            print("Wrong turn! Try again!")
+            
+    print(check_win() + 'Won!')
+        
+        
+        
+
+#python ttt-game.py
 
 print("Welcome to Tic-Tac-Toe!")
 start_game()
